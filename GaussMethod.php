a9bd843bddd7a45d2d4fc68e26607cb4c05c4185
Dmitry <usefullforit@gmail.com>
2022-11-23 23:38:24 +0300

    newton modification

--- GaussMethod.php
+++ GaussMethod.php
@@ -1,120 +1,130 @@
 <?php
 
-namespace gauss;
+namespace gauss {
 
-class GaussMethod
-{
-    private int $size;
-    private int $rowsAmount;
-    private int $columnsAmount;
-    private array $originalMatrix;
-    private array $matrix;
-    private array $x;
-
-    public function __construct($size, $matrix)
+    class GaussMethod
     {
-        $this->size = $size;
-        $this->columnsAmount = $size + 1;
-        $this->rowsAmount = $size;
-        for ($rowIndex = 0; $rowIndex < $this->rowsAmount; ++$rowIndex) {
-            $this->x[$rowIndex] = 1;
-            for ($columnIndex = 0; $columnIndex < $this->columnsAmount; ++$columnIndex) {
-                $this->matrix[$rowIndex][$columnIndex] = $matrix[$rowIndex][$columnIndex];
-                $this->originalMatrix[$rowIndex][$columnIndex] = $matrix[$rowIndex][$columnIndex];
-            }
-        }
-    }
+        private int $size;
+        private int $rowsAmount;
+        private int $columnsAmount;
+        private array $originalMatrix;
+        private array $matrix;
+        private array $x;
 
-    private function maxCurrentColumn($columnIndex): int
-    {
-        $max = $this->matrix[$columnIndex][$columnIndex];
-        $maxRowIndex = $columnIndex;
-        for ($i = $columnIndex; $i < $this->rowsAmount; ++$i) {
-            if ($max < $this->matrix[$i][$columnIndex]) {
-                $max = $this->matrix[$i][$columnIndex];
-                $maxRowIndex = $i;
-            }
-        }
-        return $maxRowIndex;
-    }
-
-    private function swapRows($columnIndex, $maxRowIndex): void
-    {
-        for ($i = $columnIndex; $i < $this->columnsAmount; ++$i) {
-            $temp = $this->matrix[$columnIndex][$i];
-            $this->matrix[$columnIndex][$i] = $this->matrix[$maxRowIndex][$i];
-            $this->matrix[$maxRowIndex][$i] = $temp;
-        }
-    }
-
-    private function dividingOnColumnElement($columnIndex): void
-    {
-        for ($i = $columnIndex; $i < $this->rowsAmount; ++$i) {
-            $delimiter = $this->matrix[$i][$columnIndex];
-            for ($j = $columnIndex; $j < $this->columnsAmount; ++$j) {
-                $this->matrix[$i][$j] /= $delimiter;
-            }
-        }
-    }
-
-    private function difCurrentRowAndMaxRow($columnIndex): void
-    {
-        for ($i = $columnIndex + 1; $i < $this->rowsAmount; ++$i) {
-            for ($j = $columnIndex; $j < $this->columnsAmount; ++$j) {
-                $this->matrix[$i][$j] -= $this->matrix[$columnIndex][$j];
-            }
-        }
-    }
-
-    private function reverseStep(): void
-    {
-        for ($i = $this->rowsAmount - 1; $i >= 0; --$i){
-            $this->x[$i] = $this->matrix[$i][$this->columnsAmount-1];
-            for ($j = 0; $j < $this->columnsAmount-1; ++$j){
-                if ($i == $j){
-                    continue;
+        public function __construct($size, $matrix)
+        {
+            $this->size = $size;
+            $this->columnsAmount = $size + 1;
+            $this->rowsAmount = $size;
+            for ($rowIndex = 0; $rowIndex < $this->rowsAmount; ++$rowIndex) {
+                $this->x[$rowIndex] = 1;
+                for ($columnIndex = 0; $columnIndex < $this->columnsAmount; ++$columnIndex) {
+                    $this->matrix[$rowIndex][$columnIndex] = $matrix[$rowIndex][$columnIndex];
+                    $this->originalMatrix[$rowIndex][$columnIndex] = $matrix[$rowIndex][$columnIndex];
                 }
-                $this->x[$i] -= $this->x[$j]*$this->matrix[$i][$j];
             }
-            $this->x[$i] /= $this->matrix[$i][$i];
         }
-    }
-    public function doGaussMethod(): void{
-        for ($i = 0; $i < $this->rowsAmount-1; ++$i){
-            $maxRowIndex = $this->maxCurrentColumn($i);
-            $this->swapRows($i,$maxRowIndex);
-            $this->dividingOnColumnElement($i);
-            $this->difCurrentRowAndMaxRow($i);
+
+        private function maxCurrentColumn($columnIndex): int
+        {
+            $max = $this->matrix[$columnIndex][$columnIndex];
+            $maxRowIndex = $columnIndex;
+            for ($i = $columnIndex; $i < $this->rowsAmount; ++$i) {
+                if ($max < $this->matrix[$i][$columnIndex]) {
+                    $max = $this->matrix[$i][$columnIndex];
+                    $maxRowIndex = $i;
+                }
+            }
+            return $maxRowIndex;
         }
-        $this->reverseStep();
-    }
-    public function printX(): void{
-        for ($i = 0; $i < $this->rowsAmount; ++$i){
-            print $this->x[$i] . " ";
+
+        private function swapRows($columnIndex, $maxRowIndex): void
+        {
+            for ($i = $columnIndex; $i < $this->columnsAmount; ++$i) {
+                $temp = $this->matrix[$columnIndex][$i];
+                $this->matrix[$columnIndex][$i] = $this->matrix[$maxRowIndex][$i];
+                $this->matrix[$maxRowIndex][$i] = $temp;
+            }
         }
-        print "\n";
-    }
-    public function printMatrices(): void
-    {
-        for ($rowIndex = 0; $rowIndex < $this->rowsAmount; ++$rowIndex) {
-            for ($columnIndex = 0; $columnIndex < $this->columnsAmount; ++$columnIndex) {
-                print $this->matrix[$rowIndex][$columnIndex] . " ";
+
+        private function dividingOnColumnElement($columnIndex): void
+        {
+            for ($i = $columnIndex; $i < $this->rowsAmount; ++$i) {
+                $delimiter = $this->matrix[$i][$columnIndex];
+                for ($j = $columnIndex; $j < $this->columnsAmount; ++$j) {
+                    $this->matrix[$i][$j] /= $delimiter;
+                }
+            }
+        }
+
+        private function difCurrentRowAndMaxRow($columnIndex): void
+        {
+            for ($i = $columnIndex + 1; $i < $this->rowsAmount; ++$i) {
+                for ($j = $columnIndex; $j < $this->columnsAmount; ++$j) {
+                    $this->matrix[$i][$j] -= $this->matrix[$columnIndex][$j];
+                }
+            }
+        }
+
+        private function reverseStep(): void
+        {
+            for ($i = $this->rowsAmount - 1; $i >= 0; --$i) {
+                $this->x[$i] = $this->matrix[$i][$this->columnsAmount - 1];
+                for ($j = 0; $j < $this->columnsAmount - 1; ++$j) {
+                    if ($i == $j) {
+                        continue;
+                    }
+                    $this->x[$i] -= $this->x[$j] * $this->matrix[$i][$j];
+                }
+                $this->x[$i] /= $this->matrix[$i][$i];
+            }
+        }
+
+        public function doGaussMethod(): void
+        {
+            for ($i = 0; $i < $this->rowsAmount - 1; ++$i) {
+                $maxRowIndex = $this->maxCurrentColumn($i);
+                $this->swapRows($i, $maxRowIndex);
+                $this->dividingOnColumnElement($i);
+                $this->difCurrentRowAndMaxRow($i);
+            }
+            $this->reverseStep();
+        }
+
+        public function printX(): void
+        {
+            for ($i = 0; $i < $this->rowsAmount; ++$i) {
+                print $this->x[$i] . " ";
             }
             print "\n";
         }
-        print "\n";
-        for ($rowIndex = 0; $rowIndex < $this->rowsAmount; ++$rowIndex) {
-            for ($columnIndex = 0; $columnIndex < $this->columnsAmount; ++$columnIndex) {
-                print $this->originalMatrix[$rowIndex][$columnIndex] . " ";
+
+        public function printMatrices(): void
+        {
+            for ($rowIndex = 0; $rowIndex < $this->rowsAmount; ++$rowIndex) {
+                for ($columnIndex = 0; $columnIndex < $this->columnsAmount; ++$columnIndex) {
+                    print $this->matrix[$rowIndex][$columnIndex] . " ";
+                }
+                print "\n";
             }
             print "\n";
+            for ($rowIndex = 0; $rowIndex < $this->rowsAmount; ++$rowIndex) {
+                for ($columnIndex = 0; $columnIndex < $this->columnsAmount; ++$columnIndex) {
+                    print $this->originalMatrix[$rowIndex][$columnIndex] . " ";
+                }
+                print "\n";
+            }
+        }
+
+        public function getSolutionVector(): array{
+            return $this->x;
         }
     }
 }
-
-$n = readline();
+/*$n = readline();
 $matrix = [[2.6,-4.5,-2,19.07],[3,3,4.3,3.21],[-6,3.5,3,-18.25]];
 $g = new GaussMethod($n, $matrix);
 $g->doGaussMethod();
 $g->printX();
 $g->printMatrices();
+*/
---
+++ NewtonMethod.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace newton;
+
+use gauss\GaussMethod;
+
+include 'GaussMethod.php';
+
+/*if ((include 'vars.php') == TRUE) {
+    echo 'OK';
+}*/
+
+final class NewtonMethod
+{
+    private const EPS = 1E-9;
+    private float $x1;
+    private float $x2;
+    private array $slae;
+
+    public function __construct($x1, $x2)
+    {
+        $this->x1 = $x1;
+        $this->x2 = $x2;
+    }
+
+    private function derF1X1($x1): float
+    {
+        return 2 * $x1;
+    }
+
+    private function derF1X2($x2): float
+    {
+        return (-1) * 2 * $x2;
+    }
+
+    private function derF2X1($x2): float
+    {
+        return $x2 * $x2 * $x2;
+    }
+
+    private function derF2X2($x1, $x2): float
+    {
+        return 3 * $x2 * $x2 * $x1 - 1;
+    }
+
+    private function f1($x1, $x2): float
+    {
+        return $x1 * $x1 - $x2 * $x2 - 1;
+    }
+
+    private function f2($x1, $x2): float
+    {
+        return $x1 * $x2 * $x2 * $x2 - $x2 - 3;
+    }
+
+    public function doNewton(): void
+    {
+        print "k x1 deltaX1  x2 deltaX2\n";
+        print 0 . " " . $this->x1 . " " . 0 . " " . $this->x2 . " " . 0 . "\n";
+        $i = 0;
+        do {
+            $this->slae[0][0] = $this->derF1X1($this->x1);
+            $this->slae[0][1] = $this->derF1X2($this->x2);
+            $this->slae[0][2] = $this->f1($this->x1, $this->x2);
+            $this->slae[1][0] = $this->derF2X1($this->x2);
+            $this->slae[1][1] = $this->derF2X2($this->x1, $this->x2);
+            $this->slae[1][2] = $this->f2($this->x1, $this->x2);
+            $g = new GaussMethod(2, $this->slae);
+            $g->doGaussMethod();
+            $delta = $g->getSolutionVector();
+            $dx1 = $delta[0];
+            $dx2 = $delta[1];
+            $this->x1 -= $dx1;
+            $this->x2 -= $dx2;
+            ++$i;
+            print $i . " " . $this->x1 . " " . $dx1 . " " . $this->x2 . " " . $dx2 . "\n";
+        } while ((abs($dx1) > NewtonMethod::EPS || abs($dx2) > NewtonMethod::EPS) and ($i < 10));
+    }
+}
+
+$n = new NewtonMethod(1, 1);
+$n->doNewton();
